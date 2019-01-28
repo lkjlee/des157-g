3611414c908d23306148ad09ad7bf27d702f8cbf
Julie Lee <lkjlee@ucdavis.edu>
2019-01-27 20:40:51 -0800

    Update on madlibs text

--- studio1/script.js
+++ studio1/script.js
@@ -15,26 +15,20 @@ function processForm() {
   var myStory = document.querySelector('#myStory');
   var storySection = document.querySelector("#storySection");
 
-
-
   myStory.innerHTML =
     "<p>Long long ago, <em>" + name + "</em> begins their journey in a(n) <em>" +
     adj + " </em>morning, when the <em>" + noun + "</em> came with a big prophecy for them. <em>" + name +
-    "</em> had to <em>" + verb + "</em> out of their hometown to set out on this adventure for the sake of saving the world from <em>"
-    + creature + "</em>. They head towards their destination and find instead an army of tiny <em>" + creature +
+    "</em> had to <em>" + verb + "</em> out of their hometown to go on this adventure in order to save the world from <em>"
+    + creature + "</em>. They head towards their destination and instead find an army of tiny <em>" + creature +
     "</em>! It was revealed that the mastermind was <em>" + noun + "</em> all along and used this prophecy as a ploy! But ultimately <em>" + name + "</em> remained <em>" + adj + "</em> and they <em>"
     + verb + "</em> their fears, saving the world once and for all. With <em>" + noun + "</em> defeated, <em>" + name +
-    "</em> made their way home with an adventure that few believed, but they'll always remember that strange adventurer and their whacky story.</p>";
+    "</em> made their way home with an adventure that few believed, but people will always remember that individual's whacky story.</p>";
 
   storySection.className = "show";
 
   return false;
 }
 
-//for drop-down menu
-// if (.select == 'cave'){
-//
-// }
 
 function resetForm() {
   storySection.className = "hide";
