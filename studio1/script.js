fee23d13d119689b2d67ee960ace50b0b675c51e
Julie <lkjlee@ucdavis.edu>
2019-01-28 11:50:41 -0800

    Class Edit 1/28/19

--- studio1/script.js
+++ studio1/script.js
@@ -3,6 +3,7 @@ console.log('reading js');
 
 document.addEventListener('submit', processForm);
 document.querySelector('#reset').onclick = resetForm;
+// document.getElementById('testImg').src = images/animation.gif;
 
 function processForm() {
   var name = document.getElementById('name').value;
@@ -10,19 +11,43 @@ function processForm() {
   var adj = document.getElementById('adj').value;
   var noun = document.getElementById('noun').value;
   var creature = document.getElementById('creature').value;
-
+  var place = document.getElementById('place').value;
 
   var myStory = document.querySelector('#myStory');
   var storySection = document.querySelector("#storySection");
 
+
+// function showImage(src, width, height, alt){
+//   var cave = document.El
+// }
+// if (place === 'cave'){
+//   document.createElement = 'testImg';
+// } else (place === 'sea') {
+//   testImg.style.none;
+// }
+
+// switch (place) {
+//    case 'cave':
+//      storySection.style.backgroundColor = 'purple';
+//      myStory.style.color = 'antiquewhite';
+//      break;
+//    case 'sea':
+//      storySection.style.backgroundColor = 'orange';
+//      myStory.style.color = 'black';
+//      break;
+//    default:
+//      storySection.style.backgroundColor = '#333';
+//      myStory.style.color = 'antiquewhite';
+//  }
+
   myStory.innerHTML =
-    "<p>Long long ago, <em>" + name + "</em> begins their journey in a(n) <em>" +
+    "<p>Long long ago, <em>" + name + "</em> begins their journey in one <em>" +
     adj + " </em>morning, when the <em>" + noun + "</em> came with a big prophecy for them. <em>" + name +
     "</em> had to <em>" + verb + "</em> out of their hometown to go on this adventure in order to save the world from <em>"
     + creature + "</em>. They head towards their destination and instead find an army of tiny <em>" + creature +
-    "</em>! It was revealed that the mastermind was <em>" + noun + "</em> all along and used this prophecy as a ploy! But ultimately <em>" + name + "</em> remained <em>" + adj + "</em> and they <em>"
+    "</em> ! It was revealed that the mastermind was <em>" + noun + "</em> all along and used this prophecy as a ploy! But ultimately <em>" + name + "</em> remained <em>" + adj + "</em> and they <em>"
     + verb + "</em> their fears, saving the world once and for all. With <em>" + noun + "</em> defeated, <em>" + name +
-    "</em> made their way home with an adventure that few believed, but people will always remember that individual's whacky story.</p>";
+    "</em> made their way home with an adventure that few believed, but people will always remember the whacky story.</p>";
 
   storySection.className = "show";
 
