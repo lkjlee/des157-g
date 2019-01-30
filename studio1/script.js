72ee9aa75055d38af0f6df6603f8f9b8c6cb35d7
Julie Lee <lkjlee@ucdavis.edu>
2019-01-30 00:09:40 -0800

    Finished madlibs studio 1 project

--- studio1/script.js
+++ studio1/script.js
@@ -1,9 +1,16 @@
 'use strict';
 console.log('reading js');
-
 document.addEventListener('submit', processForm);
 document.querySelector('#reset').onclick = resetForm;
-// document.getElementById('testImg').src = images/animation.gif;
+document.querySelector('#place').onclick = setting;
+document.getElementById('submit').value = "Let's go!";
+document.getElementById('reset').value = "Fresh page";
+var randomPlace = new Array("images/cave.png", "images/sea.png", "images/castle.png", "images/grave.png");
+
+function setting() {
+  var randomNum = Math.floor(Math.random() * randomPlace.length);
+  document.getElementById("cave").src = randomPlace[randomNum];
+}
 
 function processForm() {
   var name = document.getElementById('name').value;
@@ -11,50 +18,26 @@ function processForm() {
   var adj = document.getElementById('adj').value;
   var noun = document.getElementById('noun').value;
   var creature = document.getElementById('creature').value;
-  var place = document.getElementById('place').value;
 
   var myStory = document.querySelector('#myStory');
   var storySection = document.querySelector("#storySection");
 
-
-// function showImage(src, width, height, alt){
-//   var cave = document.El
-// }
-// if (place === 'cave'){
-//   document.createElement = 'testImg';
-// } else (place === 'sea') {
-//   testImg.style.none;
-// }
-
-// switch (place) {
-//    case 'cave':
-//      storySection.style.backgroundColor = 'purple';
-//      myStory.style.color = 'antiquewhite';
-//      break;
-//    case 'sea':
-//      storySection.style.backgroundColor = 'orange';
-//      myStory.style.color = 'black';
-//      break;
-//    default:
-//      storySection.style.backgroundColor = '#333';
-//      myStory.style.color = 'antiquewhite';
-//  }
-
   myStory.innerHTML =
     "<p>Long long ago, <em>" + name + "</em> begins their journey in one <em>" +
     adj + " </em>morning, when the <em>" + noun + "</em> came with a big prophecy for them. <em>" + name +
-    "</em> had to <em>" + verb + "</em> out of their hometown to go on this adventure in order to save the world from <em>"
-    + creature + "</em>. They head towards their destination and instead find an army of tiny <em>" + creature +
-    "</em> ! It was revealed that the mastermind was <em>" + noun + "</em> all along and used this prophecy as a ploy! But ultimately <em>" + name + "</em> remained <em>" + adj + "</em> and they <em>"
-    + verb + "</em> their fears, saving the world once and for all. With <em>" + noun + "</em> defeated, <em>" + name +
-    "</em> made their way home with an adventure that few believed, but people will always remember the whacky story.</p>";
+    "</em> had to <em>" + verb + "</em> out of their hometown to go on this adventure in order to save the world from <em>" +
+    creature + "</em>. They head towards their destination and instead find an army of tiny <em>" + creature +
+    "</em> ! It was revealed that the mastermind was their mentor, the <em>" + noun + "</em> all along! But ultimately <em>" + name + "</em> remained <em>" + adj + "</em> as they <em>" +
+    verb + "</em> both the " + noun + " and their fears, managing to barely save the world once and for all! With the <em>" + noun + "</em> soundly defeated, <em>" + name +
+    "</em> made their way home with an adventure that few believed, but a story to remember forever.</p>";
 
   storySection.className = "show";
 
   return false;
 }
 
-
 function resetForm() {
   storySection.className = "hide";
+  var randomNum = Math.floor(Math.random() * randomPlace.length);
+  document.getElementById("grave").src = randomPlace[randomNum];
 }
