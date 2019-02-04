b96f8c5cfce42ef8cec87b8319d23b05ccdc2cbe
Julie <lkjlee@gmail.com>
2019-02-04 10:03:47 -0800

    Working Javascript studio2

--- studio2/script.js
+++ studio2/script.js
@@ -1,1 +1,14 @@
 console.log('reading.js');
+
+var myBag = document.querySelector('#myBag');
+var popOut = document.querySelector('#popOut')
+var closeBag = document.querySelector('#closeBag')
+
+
+myBag.addEventListener('click',function(){
+  popOut.style.display='block';
+})
+
+closeBag.addEventListener('click', function(){
+  popOut.style.display='none';
+})
