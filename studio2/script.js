9840481558d32774a4bbbf7891fcc87ab7535636
Julie Lee <jewlzkat@scc2102-3.ucdavis.edu>
2019-02-06 11:48:25 -0800

    Finished tablet view (fixed media query)

--- studio2/script.js
+++ studio2/script.js
@@ -3,12 +3,14 @@ console.log('reading.js');
 var myBag = document.querySelector('#myBag');
 var nav = document.querySelector('nav');
 var closeBag = document.querySelector('#closeBag');
-var driftPhone = document.querySelector('#driftPhone');
+// var driftPhone = document.querySelector('#driftPhone');
 var driftGrocery = document.querySelector('#driftGrocery');
 var driftAirpods = document.querySelector('#driftAirpods');
-var li = document.querySelector('li');
-
-
+var objects = document.querySelector('objects');
+var phoneGlow =document.getElementById('phoneGlow');
+var earphonesGlow = document.getElementById('eGlow');
+var groceryGlow = document.getElementById('gGlow');
+var bagGlow = document.getElementById('bagGlow');
 // Fading mouse hover
 // li.addEventListener('mouseover',function(){
 //   li.style.opacity=1;
@@ -19,13 +21,53 @@ var li = document.querySelector('li');
 // });
 
 
-// Image hover glow
+// Image hover glow-image swap (need to adjust image sizing in photoshop)
+phoneGlow.addEventListener('mouseover', function() {
+  phoneGlow.style.opacity=1;
+  phoneGlow.src='images/phoneglow.png';
+});
 
+phoneGlow.addEventListener('mouseout', function() {
+  phoneGlow.style.opacity=.5;
+  phoneGlow.src='images/phone.png';
+});
+
+earphonesGlow.addEventListener('mouseover', function() {
+  earphonesGlow.style.opacity=1;
+  earphonesGlow.src='images/earphonesglow.png';
+});
+
+earphonesGlow.addEventListener('mouseout', function() {
+  earphonesGlow.style.opacity=.5;
+  earphonesGlow.src='images/earphones.png';
+});
+
+groceryGlow.addEventListener('mouseover', function() {
+  groceryGlow.style.opacity=1;
+  groceryGlow.src='images/groceryGlow.png';
+});
+
+groceryGlow.addEventListener('mouseout', function() {
+  groceryGlow.style.opacity=.5;
+  groceryGlow.src='images/groceryGlow.png';
+});
+
+bagGlow.addEventListener('mouseover', function() {
+  // bagHover.style.opacity=.5;
+  bagGlow.src='images/bagGlow.png';
+});
+
+bagGlow.addEventListener('mouseout', function() {
+  // bagHover.style.opacity=.5;
+  bagGlow.src='images/bag.png';
+});
 
 // Pop-up bubbles
-myBag.addEventListener('click',function(){
+bagGlow.addEventListener('click',function(){
   popOut.style.display='block';
-  driftPhone.className='phoneMove';
+  // bagGlow.src='images/bagGlow.png';
+// objects.innerHTML = "Phone";
+  // driftPhone.className='phoneMove';
 });
 
 closeBag.addEventListener('click', function(){
