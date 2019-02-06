23a7c1bf6ea4b62dcd9c044f1a75833e681bd84f
Julie Lee <lkjlee@ucdavis.edu>
2019-02-05 21:00:38 -0800

    Update from class

--- studio2/script.js
+++ studio2/script.js
@@ -1,1 +1,29 @@
 console.log('reading.js');
+
+var myBag = document.querySelector('#myBag');
+var nav = document.querySelector('nav');
+var closeBag = document.querySelector('#closeBag');
+var driftPhone = document.querySelector('#driftPhone');
+var driftGrocery = document.querySelector('#driftGrocery');
+var driftAirpods = document.querySelector('#driftAirpods');
+var ul = document.querySelector('ul');
+
+
+// Fading mouse hover
+ul.addEventListener('mouseover',function(){
+  ul.style.opacity=1;
+});
+
+ul.addEventListener('mouseout',function(){
+  ul.style.opacity=.25;
+});
+
+// Pop-up bubbles
+myBag.addEventListener('click',function(){
+  popOut.style.display='block';
+  driftPhone.className='phoneMove';
+});
+
+closeBag.addEventListener('click', function(){
+  popOut.style.display='none';
+});
