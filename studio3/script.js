2eeb378fce3fba63c213cf986d4ac718706e31a7
Julie Lee <lkjlee@ucdavis.edu>
2019-02-24 22:17:04 -0800

    Finish javascript and mobile view

--- studio3/script.js
+++ studio3/script.js
@@ -14,48 +14,47 @@ var net = document.querySelector('#net');
 var paper = document.querySelector('#paper');
 var plastic = document.querySelector('#plastic');
 
-var score = document.querySelector("#trashCleaned");
-var scoreCount = 0
+var score = document.querySelector('#trashCleaned');
+var scoreCount = 0;
 
-var reset = document.querySelector("#reset");
+var reset = document.querySelector('#reset');
 var winPattern = document.querySelector('#winPattern');
+
+winPattern.style.display = 'none';
 
 trashOpen.addEventListener('mouseover', function() {
   pointer.style.opacity = 1;
   pointer.src = 'images/pointer.png';
-  helpText.style.opacity=1;
+  helpText.style.opacity = 1;
+  if (scoreCount == '6') {
+    pointer.style.display = 'none';
+    helpText.style.display = 'none';
+  }
 });
 
 trashOpen.addEventListener('mouseout', function() {
   pointer.style.opacity = 0;
-  helpText.style.opacity=0;
+  helpText.style.opacity = 0;
   pointer.src = 'images/pointer.png';
 });
 
-function allowDrop(ev){
+function allowDrop(ev) {
   ev.preventDefault();
 }
 
 function drag(ev) {
-  ev.dataTransfer.setData("src", ev.target.id);
+  ev.dataTransfer.setData('src', ev.target.id);
 }
 
 function drop(ev) {
   ev.preventDefault();
-  var data = ev.dataTransfer.getData("src");
+  var data = ev.dataTransfer.getData('src');
   ev.target.appendChild(document.getElementById(data));
-
-}
-
-// Score Keeping
-
-boot.addEventListener("drop", updateScore);
-
-function updateScore() {
   scoreCount++;
-  score.innerHTML = "Score: " + scoreCount;
+  score.innerHTML = "Trash Left <br>" + scoreCount + " /6";
+  if (scoreCount == '6') {
+    slugSad.src = 'images/slugHappy.gif';
+    winPattern.style.display = 'block';
+    score.style.display = 'none';
+  }
 }
-
-// function draw(){
-//   ellipse(50,50,50,50);
-// }
